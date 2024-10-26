1af1110823e0c0739afecec3aa0dd468e04e2ed9
Neo <neo@Neos-Virtual-Machine.local>
2024-10-26 09:38:07 +0530

    Learn where to commit

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -3,6 +3,8 @@
 int main(){
     std::cout << "What is your name" << std::endl;
     std::cout << "my name is Adil" << std::endl;
+    std::cout << "i am learning c++" << std::endl;
+    std::cout << "i am also learnig git" << std::endl;
 
     return 0;
 }
