2bf70d06038d46211a0e5d7ef5c57d67264d410e
Neo <neo@Neos-Virtual-Machine.local>
2024-10-26 10:42:19 +0530

    Single line and Multi line comment

---
+++ comments/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    // this is an on line comment
+    /*this is multi line comment
+    another line of comment
+    ohh another line of commnet */
+    std::cout << "Hello World!" << std::endl;
+}
