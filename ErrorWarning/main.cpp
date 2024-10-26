f0a41dea919aa6693ddbc0ac84a08856f9fc3bc5
Neo <neo@Neos-Virtual-Machine.local>
2024-10-26 10:58:00 +0530

    error

---
+++ ErrorWarning/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    // compile time error
+    // std::cout << "Hello" << std::endl
+    std::cout << "Hello" << std::endl;
+    // run time error
+    /*
+        int value = 7/0;
+    */
+   return 0;
+}
